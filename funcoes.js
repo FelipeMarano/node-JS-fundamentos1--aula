233f0434ffd9606afe642c8ca0683a09779815a6
Felipe Marano <felipehenriquemarano@gmail.com>
2024-04-02 13:22:36 -0300

    relembrando funções

---
+++ funcoes.js
@@ -0,0 +1,10 @@
+// parâmetros/argumentos
+// retorno
+
+function exibeInfosNomeEstudante (nome,nota) {
+    return `o nome é ${nome} e a nota é ${nota}`; // permite que tudo dentro da função funcione do lado de fora
+    
+}
+
+console.log(exibeInfosNomeEstudante ("Caroline", 10));
+console.log(exibeInfosNomeEstudante ("Ana", 7)); 
